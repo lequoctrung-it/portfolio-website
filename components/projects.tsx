469d97641210a038b30018ae761eee768079fb69
Trung Le <leqtrungit@gmail.com>
2023-09-17 22:08:58 +0700

    Remove Projects

--- components/projects.tsx
+++ components/projects.tsx
@@ -7,8 +7,8 @@ import Project from "./project";
 import { useSectionInView } from "@/lib/hooks";
 
 export default function Projects() {
-  const { ref } = useSectionInView("Projects", 0.5);
-
+  // const { ref } = useSectionInView("Projects", 0.5);
+    const ref = null;
   return (
     <section ref={ref} id="projects" className="scroll-mt-28 mb-28">
       <SectionHeading>My projects</SectionHeading>
