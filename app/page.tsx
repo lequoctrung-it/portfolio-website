3c77837c6313caf3d9b6569bc8bf23038f22b1e0
Trung Le <leqtrungit@gmail.com>
2024-04-08 23:06:22 +0700

    Hide Contact forms

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
 import About from "@/components/about";
-import Contact from "@/components/contact";
 import Experience from "@/components/experience";
 import Intro from "@/components/intro";
-import Projects from "@/components/projects";
 import SectionDivider from "@/components/section-divider";
 import Skills from "@/components/skills";
 
@@ -15,7 +13,7 @@ export default function Home() {
       {/*<Projects />*/}
       <Skills />
       <Experience />
-      <Contact />
+      {/*<Contact />*/}
     </main>
   );
 }
